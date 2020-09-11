f072591cf5959754f5d00a666addff9386a12bc6
BevilKunG <devilbevilgamer@gmail.com>
2020-09-11 08:20:26 +0700

    set up tab navigator

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {
   createStackNavigator,
   StackNavigationProp,
 } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
 import { View, Text, Button } from 'react-native'
 
@@ -32,14 +33,15 @@ const SecondScreen = () => {
 }
 
 const Stack = createStackNavigator<RootStackParamList>()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Second" component={SecondScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Second" component={SecondScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
