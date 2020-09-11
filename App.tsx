1836a4137e945834405df8bfaa28d42b435c2bae
BevilKunG <devilbevilgamer@gmail.com>
2020-09-11 08:15:18 +0700

    set up stack navigator

--- App.tsx
+++ App.tsx
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar'
+import 'react-native-gesture-handler'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
-export default function App() {
+type RootStackParamList = {
+  Home: any
+  Second: any
+}
+
+type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>
+const HomeScreen = () => {
+  const navigation = useNavigation<HomeScreenNavigationProp>()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Go to Second" onPress={() => navigation.push('Second')} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+const SecondScreen = () => {
+  return (
+    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Second Screen</Text>
+    </View>
+  )
+}
+
+const Stack = createStackNavigator<RootStackParamList>()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
