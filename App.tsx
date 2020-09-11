4ca86800480f9f5d5823de91ceea605f6cef7c48
BevilKunG <devilbevilgamer@gmail.com>
2020-09-11 09:40:39 +0700

    split home screen to seperated component

--- App.tsx
+++ App.tsx
@@ -1,46 +1,21 @@
 import 'react-native-gesture-handler'
-import { NavigationContainer, useNavigation } from '@react-navigation/native'
-import {
-  createStackNavigator,
-  StackNavigationProp,
-} from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { View, Text, Button } from 'react-native'
 
-type RootStackParamList = {
+import { Home } from './src/screens/Home'
+
+export type RootStackParamList = {
   Home: any
-  Second: any
 }
 
-type HomeScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Home'>
-const HomeScreen = () => {
-  const navigation = useNavigation<HomeScreenNavigationProp>()
-  return (
-    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to Second" onPress={() => navigation.push('Second')} />
-    </View>
-  )
-}
-
-const SecondScreen = () => {
-  return (
-    <View style={{ flex:1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Second Screen</Text>
-    </View>
-  )
-}
-
-const Stack = createStackNavigator<RootStackParamList>()
 const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Second" component={SecondScreen} />
+        <Tab.Screen name="Home" component={Home} />
       </Tab.Navigator>
     </NavigationContainer>
   )
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { View, Text } from 'react-native'
+
+export const Home: FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  )
+}
