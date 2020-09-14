acee37defa06c06df977e22efe217686e943bc76
BevilKunG <devilbevilgamer@gmail.com>
2020-09-14 14:54:13 +0700

    try moment js update time

--- src/screens/Reward/RewardNew.tsx
+++ src/screens/Reward/RewardNew.tsx
@@ -1,8 +1,20 @@
-import React, { FC } from 'react'
-import {View} from 'react-native'
+import moment from 'moment'
+import React, { FC, useEffect, useState } from 'react'
+import { View, Text } from 'react-native'
 
 export const RewardNew: FC = () => {
+  const [time, setTime] = useState(moment().format('h:mm:ss a'))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(moment().format('h:mm:ss a'))
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <View></View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{time}</Text>
+    </View>
   )
 }
