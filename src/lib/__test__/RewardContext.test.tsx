d8f3840545b6ca3d4ac38600cd77a3ff7722f435
BevilKunG <devilbevilgamer@gmail.com>
2020-09-15 15:01:34 +0700

    add reducer delete reward test and refactor reward crud test

--- src/lib/__test__/RewardContext.test.tsx
+++ src/lib/__test__/RewardContext.test.tsx
@@ -1,25 +1,21 @@
 import { reducer, initState, RewardActionTypes, Reward } from '../RewardContext'
 
 describe('reward shared reducer testing', () => {
-  it('add reward should be add reward in rewards state', () => {
+  it('add reward should be update state', () => {
     const { rewards } = initState
     const reward: Reward = {
-      id: '1',
+      id: `${rewards.length + 1}`,
       name: 'Reward #1',
       point: 100,
     }
-
-    expect(
-      reducer(initState, {
-        type: RewardActionTypes.AddReward,
-        payload: {
-          reward,
-        },
-      }),
-    ).toEqual({
-      ...initState,
-      rewards: [...rewards, reward],
+    const resultState = reducer(initState, {
+      type: RewardActionTypes.AddReward,
+      payload: {
+        reward,
+      },
     })
+
+    expect(resultState.rewards).toContain(reward)
   })
 
   it('update reward should be update state', () => {
@@ -40,52 +36,69 @@ describe('reward shared reducer testing', () => {
     }
 
     describe('update reward name should be update state', () => {
-      expect(
-        reducer(prevState, {
-          type: RewardActionTypes.UpdateReward,
-          payload: {
-            reward: {
-              ...reward,
-              name: 'Reward #2',
-            },
+      const resultState = reducer(prevState, {
+        type: RewardActionTypes.UpdateReward,
+        payload: {
+          reward: {
+            ...reward,
+            name: 'Reward #2',
           },
-        }),
-      ).toEqual({
-        ...prevState,
-        rewards: rewards.map((item) =>
-          item.id === reward.id
-            ? {
-                ...reward,
-                name: 'Reward #2',
-              }
-            : item,
-        ),
+        },
       })
+
+      expect(resultState.rewards.find((item) => item.id === reward.id)).toEqual(
+        {
+          ...reward,
+          name: 'Reward #2',
+        },
+      )
     })
 
     describe('update reward point should be update state', () => {
-      expect(
-        reducer(prevState, {
-          type: RewardActionTypes.UpdateReward,
-          payload: {
-            reward: {
-              ...reward,
-              point: 100,
-            },
+      const resultState = reducer(prevState, {
+        type: RewardActionTypes.UpdateReward,
+        payload: {
+          reward: {
+            ...reward,
+            point: 100,
           },
-        }),
-      ).toEqual({
-        ...prevState,
-        rewards: rewards.map((item) =>
-          item.id === reward.id
-            ? {
-                ...reward,
-                point: 100,
-              }
-            : item,
-        ),
+        },
       })
+
+      expect(resultState.rewards.find((item) => item.id === reward.id)).toEqual(
+        {
+          ...reward,
+          point: 100,
+        },
+      )
     })
+  })
+
+  it('delete reward should be update state', () => {
+    const { rewards } = initState
+    const reward: Reward = {
+      id: '1',
+      name: 'Reward #1',
+      point: 20,
+    }
+
+    const notContainRewardID = !rewards.find(({ id }) => id === reward.id)
+
+    const prevState = {
+      ...initState,
+      rewards: notContainRewardID
+        ? [...rewards, reward]
+        : rewards.map((item) => (item.id === reward.id ? reward : item)),
+    }
+
+    const resultState = reducer(prevState, {
+      type: RewardActionTypes.DeleteReward,
+      payload: {
+        reward,
+      },
+    })
+
+    expect(resultState.rewards).not.toContain(reward)
   })
 
   it('edit mode state should be equal edit mode in action payload', () => {
