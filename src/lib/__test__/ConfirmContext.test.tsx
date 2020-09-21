88d25e4b398fd0381acc1addb403b6e8e4a359fa
BevilKunG <devilbevilgamer@gmail.com>
2020-09-21 21:10:16 +0700

    edit confirm challenge and confirm reward reducer  test

--- src/lib/__test__/ConfirmContext.test.tsx
+++ src/lib/__test__/ConfirmContext.test.tsx
@@ -1,8 +1,14 @@
 import { Challenge } from '../ChallengeContext'
-import { reducer, initState, ConfirmActionTypes } from '../ConfirmContext'
+import {
+  reducer,
+  initState,
+  ConfirmActionTypes,
+  ConfirmTypes,
+} from '../ConfirmContext'
+import { Reward } from '../RewardContext'
 
-describe('confirm challenge reducer test', () => {
-  it('show modal should be update state', () => {
+describe('confirm reducer test', () => {
+  it('show confirm challenge modal should be update state', () => {
     const challenge: Challenge = {
       id: '1',
       name: 'Challenge #1',
@@ -11,6 +17,7 @@ describe('confirm challenge reducer test', () => {
     const resultState = reducer(initState, {
       type: ConfirmActionTypes.ShowModal,
       payload: {
+        confirmType: ConfirmTypes.ConfirmChallenge,
         challenge,
       },
     })
@@ -19,8 +26,39 @@ describe('confirm challenge reducer test', () => {
       expect(resultState.modalVisible).toBeTruthy()
     })
 
+    describe('confirm type should not be change', () => {
+      expect(resultState.confirmType).toEqual(ConfirmTypes.ConfirmChallenge)
+    })
+
     describe('modal should contain challenge detail', () => {
       expect(resultState.challenge).not.toBeNull()
+    })
+  })
+
+  it('show confirm reward modal should be update state', () => {
+    const reward: Reward = {
+      id: '1',
+      name: 'Reward #1',
+      point: 100,
+    }
+    const resultState = reducer(initState, {
+      type: ConfirmActionTypes.ShowModal,
+      payload: {
+        confirmType: ConfirmTypes.ConfirmReward,
+        reward,
+      },
+    })
+
+    describe('modal should be visible', () => {
+      expect(resultState.modalVisible).toBeTruthy()
+    })
+
+    describe('confirm type should not be change', () => {
+      expect(resultState.confirmType).toEqual(ConfirmTypes.ConfirmReward)
+    })
+
+    describe('modal should contain challenge detail', () => {
+      expect(resultState.reward).not.toBeNull()
     })
   })
 
